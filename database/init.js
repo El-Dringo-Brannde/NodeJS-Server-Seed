b85a828c6c4c86cd224d9637c403975a80d33508
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2018-01-06 01:11:07 -0800

    Moved mongo connection option to use async await

--- database/init.js
+++ database/init.js
@@ -1,9 +1,6 @@
 var config = require('./../config/constants');
 
-module.exports = mongo => {
-   return new Promise((res, rej) => {
-      mongo.connect(config.mongoURL, (err, database) => {
-         res(database.db(config.databaseName));
-      });
-   });
+module.exports = async mongo => {
+   let client = await mongo.connect(config.mongoURL);
+   return client.db(config.databaseName);
 };
